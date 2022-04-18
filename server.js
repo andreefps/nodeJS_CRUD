56b2430d387964cd9c72611f28da3097574c4d32
Andre Possiedi <andreefps@gmail.com>
2022-04-18 20:06:31 -0300

    unused file

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-// Create a local server to receive data from
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-  })
-  .listen(4001, () => console.log("Running at :4001"));
