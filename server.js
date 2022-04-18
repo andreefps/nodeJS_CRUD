47d33466aa427fa7bd8d3d87149a52ad14be7d20
Andre Possiedi <andreefps@gmail.com>
2022-04-18 20:03:08 -0300

    remove unused routes

--- server.js
+++ server.js
@@ -4,26 +4,5 @@ const http = require("http");
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
-    if (req.url.toLowerCase() === "/users") {
-      res.end(
-        JSON.stringify({
-          data: "Users Route!",
-        })
-      );
-    }
-    if (req.url.toLowerCase() === "/products") {
-      res.end(
-        JSON.stringify({
-          data: "products Route!",
-        })
-      );
-    }
-    if (req.url.toLowerCase() === ("/home" || "")) {
-      res.end(
-        JSON.stringify({
-          data: "Home Route!",
-        })
-      );
-    }
   })
   .listen(4001, () => console.log("Running at :4001"));
